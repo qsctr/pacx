2781d2e75ab40f52e3db79872023da461ec12a6b
Bretton Chen <19111183@tasfiledomain.tas.edu.tw>
2017-10-01 09:59:01 +0800

    Add stuff

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
+var home = document.querySelector('#home');
+var about = document.querySelector('#about');
+
 addEventListener('load', function () {
     document.body.classList.add('loaded');
 });
 
+var currentDetail = document.querySelector('#feature-detail');
+var currentCircle;
+
 Array.prototype.slice.call(document.querySelectorAll('.feature-circle')).forEach(function (circle) {
     circle.addEventListener('mouseenter', function () {
-        circle.classList.remove('mdc-elevation--z2');
-        circle.classList.add('mdc-elevation--z8');
+        if (circle !== currentCircle) {
+            circle.classList.remove('mdc-elevation--z2');
+            circle.classList.add('mdc-elevation--z8');
+        }
     });
     circle.addEventListener('mouseleave', function () {
-        circle.classList.remove('mdc-elevation--z8');
-        circle.classList.add('mdc-elevation--z2');
+        if (circle !== currentCircle) {
+            circle.classList.remove('mdc-elevation--z8');
+            circle.classList.add('mdc-elevation--z2');
+        }
     });
     circle.addEventListener('click', function () {
-        circle.classList.add('expanded');
+        if (currentCircle) {
+            console.log(currentCircle);
+            currentCircle.classList.remove('mdc-elevation--z8');
+        }
+        currentCircle = circle;
+        circle.classList.add('mdc-elevation--z8');
+        currentDetail.classList.add('hidden');
+        var prevDetail = currentDetail;
+        currentDetail = document.querySelector('#' + circle.id + '-detail');
+        setTimeout(function () {
+            prevDetail.style.display = 'none';
+            currentDetail.style.display = 'block';
+            currentDetail.offsetHeight;
+            currentDetail.classList.remove('hidden');
+        }, 500);
     });
 });
